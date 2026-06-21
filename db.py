f9a350bc249f20f1690ac9f3b1315552781dc778
hsty01009-create <hsty01009@gmail.com>
2026-06-21 16:36:07 +0330

    Update db.py

--- db.py
+++ db.py
@@ -1,14 +1,11 @@
 import sqlite3
-import os
 
 DB = "bot.db"
-
 
 def conn():
     c = sqlite3.connect(DB)
     c.row_factory = sqlite3.Row
     return c
-
 
 def init():
     c = conn()
@@ -19,18 +16,14 @@ def init():
         user_id INTEGER PRIMARY KEY,
         username TEXT,
         first_name TEXT,
-        email TEXT,
         language TEXT DEFAULT 'fa',
         points INTEGER DEFAULT 0,
-        invited_count INTEGER DEFAULT 0,
-        referral TEXT,
-        first_start INTEGER DEFAULT 1
+        invited_count INTEGER DEFAULT 0
     )
     """)
 
     c.commit()
     c.close()
-
 
 def create_user(user_id, username, first_name):
     c = conn()
@@ -38,20 +31,18 @@ def create_user(user_id, username, first_name):
 
     cur.execute("""
     INSERT OR IGNORE INTO users
-    (user_id, username, first_name, referral)
-    VALUES (?, ?, ?, ?)
-    """, (user_id, username, first_name, str(user_id)))
+    (user_id, username, first_name)
+    VALUES (?, ?, ?)
+    """, (user_id, username, first_name))
 
     c.commit()
     c.close()
-
 
 def get_user(user_id):
     c = conn()
     cur = c.cursor()
     cur.execute("SELECT * FROM users WHERE user_id=?", (user_id,))
     return cur.fetchone()
-
 
 def update(field, value, user_id):
     c = conn()
@@ -60,13 +51,11 @@ def update(field, value, user_id):
     c.commit()
     c.close()
 
-
 def add_points(user_id, amount):
     c = conn()
     cur = c.cursor()
     cur.execute("UPDATE users SET points = points + ? WHERE user_id=?", (amount, user_id))
     c.commit()
     c.close()
-
 
 init()
